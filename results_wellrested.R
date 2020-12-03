75621696033bb57c6c8bf4039aaf44043d0afe52
Bernadine-17 <60450749+Bernadine-17@users.noreply.github.com>
2020-12-02 18:20:23 -0600

    R code
    analyzes well rested values

---
+++ results_wellrested.R
@@ -0,0 +1,57 @@
+# stores data for each subject
+
+divya_kashyap <- wellrested$test[wellrested$name=='Divya Kashyap']
+gabby_colon <- wellrested$test[wellrested$name=='Gabby Colon']
+molly_schaff <- wellrested$test[wellrested$name=='Molly Schaff']
+kushaal_vaidya <- wellrested$test[wellrested$name=='Kushaal Vaidya']
+reagan_price_computer <- wellrested$test[wellrested$name=='Reagan (computer)']
+reagan_price_phone <- wellrested$test[wellrested$name=='Reagan (phone test)']
+avery_delost <- wellrested$test[wellrested$name=='Avery Delost']
+reagan_price_ipad <- wellrested$test[wellrested$name=='Reagan (iPad)']
+reagan_price <- wellrested$test[wellrested$name=='Reagan Price']
+salma_amor <- wellrested$test[wellrested$name=='Salma Amor']
+presley_jones <- wellrested$test[wellrested$name=='Presley Jones']
+vaishnavi_kothakonda <- wellrested$test[wellrested$name=='Vaishnavi Kothakonda']
+riley_garrett <- wellrested$test[wellrested$name=='Riley Garrett']
+michael_tran <- wellrested$test[wellrested$name=='Michael Tran']
+andrew_chahda <- wellrested$test[wellrested$name=='Andrew Chahda']
+libby_banks <- wellrested$test[wellrested$name=='Libby Banks']
+sasha_roberts <- wellrested$test[wellrested$name=='Sasha Roberts']
+timothy_riedel <- wellrested$test[wellrested$name=='Timothy Riedel']
+erisa_stokes <- wellrested$test[wellrested$name=='Erisa Stokes']
+noah_haas <- wellrested$test[wellrested$name=='Noah Haas']
+valerie_foster <- wellrested$test[wellrested$name=='Valerie Foster']
+susan_vu <- wellrested$test[wellrested$name=='Susan Vu']
+bernadine_decapia <- wellrested$test[wellrested$name=='Bernadine Decapia']
+arthur_williamson <- wellrested$test[wellrested$name=='Arthur Williamson']
+ria_goyal <- wellrested$test[wellrested$name=='Ria Goyal']
+bunsri_patel <- wellrested$test[wellrested$name=='Bunsri Patel']
+
+# calculates the average baseline for each subject
+mean(divya_kashyap)
+mean(gabby_colon)
+mean(molly_schaff)
+mean(kushaal_vaidya)
+mean(reagan_price_computer)
+mean(reagan_price_phone)
+mean(avery_delost)
+mean(reagan_price_ipad)
+mean(reagan_price)
+mean(salma_amor)
+mean(presley_jones)
+mean(vaishnavi_kothakonda)
+mean(riley_garrett)
+mean(michael_tran)
+mean(andrew_chahda)
+mean(libby_banks)
+mean(sasha_roberts)
+mean(timothy_riedel)
+mean(erisa_stokes)
+mean(noah_haas)
+mean(valerie_foster)
+mean(susan_vu)
+mean(bernadine_decapia)
+mean(angela_owe)
+mean(arthur_williamson)
+mean(ria_goyal)
+mean(bunsri_patel)
